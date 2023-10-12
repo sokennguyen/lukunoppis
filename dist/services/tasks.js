02ce691aac0c612f93d59af8b48645d6d6270407
soken <nsongkien@gmail.com>
2023-10-12 15:29:49 +0300

    note

--- dist/services/tasks.js
+++ dist/services/tasks.js
@@ -2,7 +2,7 @@
 import axios from "https://cdn.skypack.dev/axios"
 const baseUrl = '/api/tasksets'
 
-
+//normal import to work with dynamic import
 export const create = async (newObject) => {
     const request = await axios.post(baseUrl,newObject)
     return request.data
